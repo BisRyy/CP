3bad7b49932e6067800c94938ee45946a1db3c39
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2022-10-13 18:56:00 +0900

    Create Min Stack.cpp

---
+++ Min Stack.cpp
@@ -0,0 +1,38 @@
+class MinStack {
+
+private:
+    stack<int> stk;
+    stack<pair<int, int>> minStk;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        stk.push(val);
+        
+        if (minStk.empty() || val < minStk.top().first) {
+            minStk.push({val, 1});
+        } else if (val == minStk.top().first) {
+            minStk.top().second++;
+        }
+    }
+    
+    void pop() {
+        if (stk.top() == minStk.top().first) {
+            minStk.top().second--;
+            if (minStk.top().second == 0) {
+                minStk.pop();
+            }
+        }
+        stk.pop();
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+        return minStk.top().first;
+    }
+};
