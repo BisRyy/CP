5950de55ce94a1133598f58909979258be284e87
Bisrat Kebere <87482476+BisRyy@users.noreply.github.com>
2022-10-14 17:53:12 +0900

    Create Move Zeroes.cpp

---
+++ Move Zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0, j=0;
+        while(i<=j && j<nums.size()){
+            if(nums[j]==0)
+                j++;
+            else
+                swap(nums[i++], nums[j++]);
+        }
+    }
+};
